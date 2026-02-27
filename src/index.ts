7cce4ff4963cdb8622b65bf1171bb752a66c2156
brenomatos-commits <breno.matos@estudante.ifms.edu.br>
2026-02-27 11:47:33 -0400

    Aula 27/02/2026

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,52 @@
-import express from 'express'
+let numero:number = 10
+let palavra:string = "algum texto";
+let bol:boolean = true
 
-const app = express()
-app.use(express.json())
+let x:number|string = 10
+x = "Breno"
 
-app.get("/",(req,res)=> {
-    res.send("Deu certo🎉🎉🎉🎉🎉🎉🎉🎉")
-})
+let vetor: number[] = [1,2,3,4,5]
+vetor[2] = 10
+vetor.push(6)
+console.log(vetor)
+//vetor = [5,6,7,8,9]
 
-app.listen(8000, () => {
-    console.log("🚀 Servidor online em http://localhost:8000");
-});
+//vetor de string
+
+let vetorString:string[] = ["a","b","ab"]
+
+interface Pessoa {
+    id?:number|string;
+    nome:string;
+    idade:number
+}
+
+const pessoa:Pessoa ={
+    nome:"Breno",
+    idade:17,
+}
+
+const pessoa2:Pessoa ={
+    nome:"Pedro",
+    idade:17,
+}
+
+pessoa.nome ="Breninho"
+
+
+const cloudVoadora:any = []
+
+const batataVoadora:undefined = undefined
+
+// import express from 'express'
+
+// const app = express()
+// app.use(express.json())
+
+// app.get("/",(req,res)=> {
+//     res.send("Deu certo🎉🎉🎉🎉🎉🎉🎉🎉")
+// })
+
+// app.listen(8000, () => {
+//     console.log("🚀 Servidor online em http://localhost:8000");
+// });

18dd95afd5ca25909f84562d0ae9fe8201c2d473
brenomatos-commits <breno.matos@estudante.ifms.edu.br>
2026-03-03 08:25:11 -0400

    Aula 03/03/2026

--- src/index.ts
+++ src/index.ts
@@ -38,6 +38,37 @@ const cloudVoadora:any = []
 
 const batataVoadora:undefined = undefined
 
+const func = (nome:string):string =>`${nome} Lindo :3`
+func("Breno")
+
+function somaVetor(v:number[]){
+    const soma = v.reduce((prev,curr)=>prev+curr,0)
+    return soma
+}
+
+// function somaVetor2(v:number[]):number{
+//     let soma = 0
+//     for (let i = 0; i< v.length; i++){
+//         soma = soma + v[i]!
+//     }
+//     return soma
+// }
+const result = somaVetor ([1,2,3])
+console.log(result)
+
+//faça uma função que receba um vetor e devolva a soma
+//dos valores pares.
+
+function somaPar(v:number[]):number{
+    let soma = 0
+    for (let i = 0; i< v.length; i++){
+        if(v[i]!%2 == 0){
+            soma = soma + v[i]!
+        }
+    }
+    return soma
+}
+console.log(`O valor da soma par é ${somaPar([1,2,3])}`)
 // import express from 'express'
 
 // const app = express()

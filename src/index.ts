2296e48cd5b7c4df49b17f355443fb9fec504ad1
brenomatos-commits <breno.matos@estudante.ifms.edu.br>
2026-03-06 11:41:21 -0400

    06/03

--- src/index.ts
+++ src/index.ts
@@ -1,75 +1,75 @@
-let numero:number = 10
-let palavra:string = "algum texto";
-let bol:boolean = true
+// let numero:number = 10
+// let palavra:string = "algum texto";
+// let bol:boolean = true
 
-let x:number|string = 10
-x = "Breno"
+// let x:number|string = 10
+// x = "Breno"
 
-let vetor: number[] = [1,2,3,4,5]
-vetor[2] = 10
-vetor.push(6)
-console.log(vetor)
-//vetor = [5,6,7,8,9]
+// let vetor: number[] = [1,2,3,4,5]
+// vetor[2] = 10
+// vetor.push(6)
+// console.log(vetor)
+// //vetor = [5,6,7,8,9]
 
-//vetor de string
+// //vetor de string
 
-let vetorString:string[] = ["a","b","ab"]
+// let vetorString:string[] = ["a","b","ab"]
 
-interface Pessoa {
-    id?:number|string;
-    nome:string;
-    idade:number
-}
+// interface Pessoa {
+//     id?:number|string;
+//     nome:string;
+//     idade:number
+// }
 
-const pessoa:Pessoa ={
-    nome:"Breno",
-    idade:17,
-}
+// const pessoa:Pessoa ={
+//     nome:"Breno",
+//     idade:17,
+// }
 
-const pessoa2:Pessoa ={
-    nome:"Pedro",
-    idade:17,
-}
+// const pessoa2:Pessoa ={
+//     nome:"Pedro",
+//     idade:17,
+// }
 
-pessoa.nome ="Breninho"
+// pessoa.nome ="Breninho"
 
 
-const cloudVoadora:any = []
+// const cloudVoadora:any = []
 
-const batataVoadora:undefined = undefined
+// const batataVoadora:undefined = undefined
 
-const func = (nome:string):string =>`${nome} Lindo :3`
-func("Breno")
+// const func = (nome:string):string =>`${nome} Lindo :3`
+// func("Breno")
 
-function somaVetor(v:number[]){
-    const soma = v.reduce((prev,curr)=>prev+curr,0)
-    return soma
-}
+// function somaVetor(v:number[]){
+//     const soma = v.reduce((prev,curr)=>prev+curr,0)
+//     return soma
+// }
 
-// function somaVetor2(v:number[]):number{
+// // function somaVetor2(v:number[]):number{
+// //     let soma = 0
+// //     for (let i = 0; i< v.length; i++){
+// //         soma = soma + v[i]!
+// //     }
+// //     return soma
+// // }
+// const result = somaVetor ([1,2,3])
+// console.log(result)
+
+// //faça uma função que receba um vetor e devolva a soma
+// //dos valores pares.
+
+// function somaPar(v:number[]):number{
 //     let soma = 0
 //     for (let i = 0; i< v.length; i++){
-//         soma = soma + v[i]!
+//         if(v[i]!%2 == 0){
+//             soma = soma + v[i]!
+//         }
 //     }
 //     return soma
 // }
-const result = somaVetor ([1,2,3])
-console.log(result)
-
-//faça uma função que receba um vetor e devolva a soma
-//dos valores pares.
-
-function somaPar(v:number[]):number{
-    let soma = 0
-    for (let i = 0; i< v.length; i++){
-        if(v[i]!%2 == 0){
-            soma = soma + v[i]!
-        }
-    }
-    return soma
-}
-console.log(`O valor da soma par é ${somaPar([1,2,3])}`)
-// import express from 'express'
+// console.log(`O valor da soma par é ${somaPar([1,2,3])}`)
+// // import express from 'express'
 
 // const app = express()
 // app.use(express.json())
@@ -81,3 +81,32 @@ console.log(`O valor da soma par é ${somaPar([1,2,3])}`)
 // app.listen(8000, () => {
 //     console.log("🚀 Servidor online em http://localhost:8000");
 // });
+
+/**
+ * Exercício 02 - divisivelPor11
+ * Nome da função - divisivelPor11
+ * Crie uma função que retorna um array com os números divisíveis por 11 no intervalo
+ * @param {number} min Número mínimo
+ * @param {number} max Número máximo
+ * @returns {number[]} Retorna um array com os números divisíveis por 11 no intervalo
+ * @example
+ *  divisivelPor11(1, 100) // [11, 22, 33, 44, 55, 66, 77, 88, 99]
+ *  
+ * divisivelPor11(11, 110) // [11, 22, 33, 44, 55, 66, 77, 88, 99, 110]
+ */
+
+//Início do seu código
+
+function divisivelPor11(min:number, max:number){
+    const vetorDivisiveis11:number[] = []
+    for (let i = min; i <= max; i++){
+        if (i%11===0){
+                vetorDivisiveis11.push(i)
+        }
+    }
+    return vetorDivisiveis11
+}
+console.log(divisivelPor11(1,100))
+
+
+//Fim do seu código

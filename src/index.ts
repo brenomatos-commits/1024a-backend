d5eadb73c14fe41cd7b625d52d96ec0fcff9ea57
brenomatos-commits <breno.matos@estudante.ifms.edu.br>
2026-03-24 08:27:12 -0400

    Aula 24/03/2026

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,46 @@
 // Get the client
-import mysql from 'mysql2/promise';
+import mysql, { type RowDataPacket } from 'mysql2/promise';
 
-// Create the connection to database
-const connection = await mysql.createConnection({
+
+import express from 'express'
+import type { Connection } from 'mysql2';
+const app = express()
+//Como criar uma rota no express?
+app.get("/pessoas", async (req,res)=>{let connection:Connection|null = null
+try {
+  const connection = await mysql.createConnection({
   host: 'localhost',
   user: 'root',
   database: 'luademel',
 });
-
-// Using placeholders
-try {
-  const queryPrepare = await connection.prepare(
-    'SELECT * FROM `pessoa`'
-  );
-  const results = await queryPrepare.execute([])
-
-  console.log(results);
+//  const result =
+//    await connection
+//    .execute('INSERT INTO pessoa (id,nome) VALUES (?,?)',[3,"Maria"])
+//  console.log(result)
+  const [dados,campos] = await connection.execute<IPessoa[]>('SELECT * FROM pessoa')
+  console.log(dados[0]);
+  for(let i = 0; i < dados.length; i++){
+    const element = dados[i];
+    console.log(element?.id,element?.nome)
+  }
+  res.status(200).json(dados)
 } catch (err) {
+  //todo
   console.log(err);
+  if(connection){
+    await (connection as Connection).end()
+  }
 }
-
 // Close the connection
-await connection.end();
 
-// create database luademel;
-// create table luademel.pessoa(
-// 	id int primary key,
-//     nome varchar(50)
-// );
-// insert into luademel.pessoa values (1, "tere");
+})
+app.post("/pessoas",(req,res)=>{})
+app.listen(8000,()=>{
+  console.log("iniciando o servidor na porta 8000")
+})
+
+
+interface IPessoa extends RowDataPacket{
+    id:number,
+    nome:string,
+}
